674f2ab114bb90a35572dc3521e3082a88632058
vrusanescu <vrusanescu@bitdefender.com>
2017-09-30 19:28:55 +0300

    Add some style and remove statusBar and splashScreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
 
 import { Geolocation } from '@ionic-native/geolocation';
@@ -30,8 +28,6 @@ import { WeatherService } from '../services/weather.service';
     HomePage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     Geolocation,
     GeolocationService,
